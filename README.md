40383579a052860a563a999f03a2f173f0b5727f
Linus G Thiel <linus@hanssonlarsson.se>
2011-07-22 17:18:37 +0200

    initial commit, v0.1.0

---
+++ README.md
@@ -0,0 +1,60 @@
+tfidf
+=====
+
+`tfidf` is a simple text frequency-inverse document frequency library for Node.js.
+
+## Version
+0.1.0
+
+## Installation
+
+    npm install tfidf
+
+## Usage
+    var tfidf = require("tfidf");
+
+    // Analyze the data
+    var data = tfidf.analyze("japherwocky", [doc1, doc2, doc3]);
+
+    // Get tfidf for a document
+    console.log(data.tfidf(doc1));
+
+    // Get the analyzed corpus as JSON, for later use
+    fs.writeFileSync("japherwockyFrequency.json", data.asJSON(), "utf8");
+
+    // Load the analyzed corpus again, later
+    var data = tfidf.analyze(fs.readFileSync("japherwockyFrequency.json"), "utf8");
+
+## Credits
+
+Linus G Thiel &lt;linus@hanssonlarsson.se&gt;
+
+## Thank you
+
+- [Ryan Dahl](http://github.com/ry) for the awesome Node.js
+- [Caolan McMahon](http://github.com/caolan) for Nodeunit
+
+## License 
+
+(The MIT License)
+
+Copyright (c) 2010 Hansson &amp; Larsson &lt;info@hanssonlarsson.se&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ spec/tfidf.js
@@ -0,0 +1,80 @@
+tfidf = require("../lib/tfidf");
+
+exports.testTf = function(test) {
+    test.expect(1);
+    test.equal(tfidf.tf("foo", "foo bar foo qux quux foo baz"), 3 / 7);
+    test.done();
+};
+
+exports.testIdf = function(test) {
+    test.expect(1);
+    test.equal(tfidf.idf(1e7, 999), 4);
+    test.done();
+};
+
+exports.testAnalyze = function(test) {
+    test.expect(1);
+    var corpus = [
+        "foo bar", "baz qux", "qux quuux", "bar baz", "bar qux",
+        "bar quux", "qux bar", "quux bar", "quux baz", "bar",
+        "qux", "quux", "padme hum", "mane hum", "fie foe",
+        "fee fie", "fie fee", "baz foe", "fie bar", "frompel hompel"
+    ];
+    var data = tfidf.analyze("foo", corpus);
+    test.equal(data.tfidf("foo bar"), 0.5);
+    test.done();
+};
+
+exports.testAnalyzeAndSave = function(test) {
+    test.expect(1);
+    var corpus = [
+        "foo bar", "baz qux", "qux quuux", "bar baz", "bar qux",
+        "bar quux", "qux bar", "quux bar", "quux baz", "bar",
+        "qux", "quux", "padme hum", "mane hum", "fie foe",
+        "fee fie", "fie fee", "baz foe", "fie bar", "frompel hompel"
+    ];
+    var data = tfidf.analyze("foo", corpus),
+        json = data.asJSON();
+    test.ok(JSON.parse(json));
+    test.done();
+};
+
+exports.testAnalyzeSerializedCorpus = function(test) {
+    test.expect(1);
+    var corpus = [
+        "foo bar", "baz qux", "qux quuux", "bar baz", "bar qux",
+        "bar quux", "qux bar", "quux bar", "quux baz", "bar",
+        "qux", "quux", "padme hum", "mane hum", "fie foe",
+        "fee fie", "fie fee", "baz foe", "fie bar", "frompel hompel"
+    ];
+    var json = tfidf.analyze("foo", corpus).asJSON(),
+        data = tfidf.analyze(json);
+    test.equal(data.tfidf("foo bar"), 0.5);
+    test.done();
+};
+
+exports.testAnalyzeWithOneNewDoc = function(test) {
+    test.expect(1);
+    var corpus = [
+        "baz qux", "qux quuux", "bar baz", "bar qux",
+        "bar quux", "qux bar", "quux bar", "quux baz", "bar",
+        "qux", "quux", "padme hum", "mane hum", "fie foe",
+        "fee fie", "fie fee", "baz foe", "fie bar", "frompel hompel"
+    ];
+    var data = tfidf.analyze("foo", corpus);
+    test.equal(data.tfidf("foo baz"), 0.5);
+    test.done();
+};
+
+exports.testAnalyzeWithTwoIdenticalDocs = function(test) {
+    test.expect(1);
+    var corpus = [
+        "foo bar", "baz qux", "baz qux", "qux quuux", "bar baz", "bar qux",
+        "bar quux", "qux bar", "quux bar", "quux baz", "bar",
+        "qux", "quux", "padme hum", "mane hum", "fie foe",
+        "fee fie", "fie fee", "baz foe", "fie bar", "frompel hompel"
+    ];
+    var data = tfidf.analyze("foo", corpus);
+    test.equal(data.tfidf("foo bar"), 0.5);
+    test.done();
+};
